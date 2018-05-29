3bb08412b4b7ecbc2ceabddb64bb158b1570ea2b
郑军 <zhengjun1987@outlook.com>
2018-05-29 23:53:08 +0800

    KotlinInAction chapter02 2018-05-29 23:53:00

--- src/chapter02kotlin_basis/TypeAndAttrs.kt
+++ src/chapter02kotlin_basis/TypeAndAttrs.kt
@@ -19,12 +19,12 @@ class Person {
 
 class Person2(
         val name: String,
-        var isMarried:Boolean
+        var isMarried: Boolean
 )
 
-fun main(args:Array<String>){
-    val person = Person2("Bob",true)
-    var person2 = Person2(isMarried = false,name = "Alex")
+fun main(args: Array<String>) {
+//    val person = Person2("Bob", true)
+    var person2 = Person2(isMarried = false, name = "Alex")
     println("person2 = ${person2}")
 //    person2 = chapter02kotlin_basis.Person2@2f410acf  //默认的toString方法没有复写
     println("person2.name = ${person2.name}")
@@ -34,4 +34,39 @@ fun main(args:Array<String>){
     person2.isMarried = true
     println("person2.isMarried = ${person2.isMarried}")
 //    person2.isMarried = true
+
+
+    val rect = Rectangle(13,13)
+    rect.tag = "silence"
+    println("rect = ${rect}")
+//    rect = Rectangle(height=13, width=13, isSquare=true, tag=immutable)
+    rect.width = 12
+    println("rect = ${rect}")
+//    rect = Rectangle(height=13, width=12, isSquare=false, tag=immutable)  由于tag变量的get属性已经被写死，所以无法显示其真正的值
+    rect.showTag()
+//    tag = immutable
 }
+
+class Rectangle(val height: Int, var width: Int) {
+    val isSquare: Boolean
+        get() {
+            return width == height
+        }
+
+    var tag : String = ""
+        get() {
+            print("get ")
+            return "immutable"}
+
+    fun showTag(): Void? {
+        println("tag = $tag")
+        return null
+    }
+
+    override fun toString(): String {
+        return "Rectangle(height=$height, width=$width, isSquare=$isSquare, tag=$tag)"
+    }
+
+
+}
+
