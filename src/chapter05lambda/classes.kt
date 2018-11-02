2216ed0be6437e907069536c897b2d63c09f5e16
Zheng Jun <zhengjun1987@outlook.com>
2018-11-02 20:29:04 +0800

    chapter05 lambda表达式 2018-11-2 20:28:52

--- src/chapter05lambda/classes.kt
+++ src/chapter05lambda/classes.kt
@@ -1,6 +1,8 @@
 package chapter05lambda
 
 import java.awt.Button
+import java.awt.event.ActionListener
+import java.io.File
 
 class Person(val name: String, val age: Int) {
     override fun toString(): String {
@@ -121,14 +123,102 @@ fun main(args: Array<String>) {
     println("flatMap = $flatMap")
 
     println("books.flatMap(Book::authors) = ${books.flatMap(Book::authors)}")
-    println("books.flatMap(Book::name) = ${books.flatMap{ it.name.asIterable() }}")
+    println("books.flatMap(Book::name) = ${books.flatMap { it.name.asIterable() }}")
 
     println("listOf(\"abc\",\"def\").flatMap(String::toList) = ${listOf("abc", "def").flatMap(String::toList)}")
 
     println("books.map(Book::authors).flatten() = ${books.map(Book::authors).flatten()}")
+
+    val sequence = books.asSequence().map { println("map = $it");it.authors }.filter { println("filter = $it");it.size > 1 }
+    println("sequence = ${sequence.javaClass.canonicalName}")
+    println("sequence = ${sequence.flatten().toList()}")
+
+    val ub = kotlin.collections.listOf(1, 2, 3, 4)
+
+    intList.asSequence()
+            .map { print("map($it) ");it * it }
+            .filter { print("filter($it) "); it % 2 == 0 }
+    println("=======================================================")
+    intList.asSequence()
+            .map { print("map($it) ");it * it }
+            .filter { print("filter($it) "); it % 2 == 0 }
+            .toList()
+    println()
+    println("=======================================================")
+    intList.asSequence()
+            .filter { print("filter($it) "); it % 2 == 0 }
+            .map { print("map($it) ");it * it }
+            .toList()
+    println()
+    println("=======================================================")
+    intList.map { print("map($it) ");it * it }
+            .filter { print("filter($it) "); it % 2 == 0 }
+            .toList()
+    println()
+    println("=======================================================")
+
+    val naturalNumbers = generateSequence(0) { it + 1 }
+    val numbersTo100 = naturalNumbers.takeWhile { it <= 99 }
+    println("numbersTo100 = ${numbersTo100.sum()}")
+
+    println("File() = ${File("C:\\Users\\Administrator\\IdeaProjects\\KotlinInAction\\.idea\\libraries\\KotlinJavaRuntime__2_.xml").isInsideHiddenDirectory()}")
+    println(File("C:\\").isHidden)
+    println(File("C:\\Users\\Administrator\\IdeaProjects\\KotlinInAction\\.idea").isHidden)
+
+    val button = Button()
+    button.addActionListener { println("it = $it") }
+
+    val listener = ActionListener { view ->
+        when (view.id) {
+            3006 -> {
+            }
+            else -> {
+            }
+        }
+    }
+
+    println("alphabet() = ${alphabet()}")
+    println("alphabet1() = ${alphabet1()}")
+    println("alphabet2() = ${alphabet2()}")
+    println("alphabet3() = ${alphabet3()}")
 }
 
-class Book(val name: String,val authors:List<String>)
+fun alphabet(): String {
+    val stringBuffer = StringBuffer()
+    return with(stringBuffer) {
+        for (c in 'A'..'Z') {
+            append(c)
+        }
+        append("\nNow I know the alphabet!")
+        toString()
+    }
+}
+
+fun alphabet1(): String = with(StringBuffer()) {
+    for (c in 'A'..'Z') {
+        append(c)
+    }
+    append("\nNow I know the alphabet!")
+    toString()
+}
+
+fun alphabet2():String = StringBuffer().apply{
+    for (c in 'A'..'Z') {
+        append(c)
+    }
+    append("\nNow I know the alphabet!")
+}.toString()
+
+fun alphabet3():String = buildString {
+    for (c in 'A'..'Z') {
+        append(c)
+    }
+    append("\nNow I know the alphabet!")
+}
+
+fun File.isInsideHiddenDirectory() = generateSequence(this) { println("file = ${it.absolutePath}");it.parentFile }.find { it.isRooted }
+
+class Book(val name: String, val authors: List<String>)
 
 fun salute() {
     println("Salute!")
@@ -184,3 +274,38 @@ fun Clerk.isAdult() = age >= 18
 //        list.groupBy(Person::age) = {29=[Person(name='Alice', age=29)], 31=[Person(name='Bob', age=31), Person(name='ZhengJun', age=31)], 43=[Person(name='Frank', age=43)]}
 //        letters.groupBy(String::first) = {a=[a, ab], b=[b]}
 //        letters.groupBy(String::last) = {a=[a], b=[ab, b]}
+//        flatMap = java.util.ArrayList
+//        flatMap = [曹雪芹, 高鹗, 罗贯中, 施耐庵, 金庸, 倪匡, 紫金陈]
+//        books.flatMap(Book::authors) = [曹雪芹, 高鹗, 罗贯中, 施耐庵, 金庸, 倪匡, 紫金陈]
+//        books.flatMap(Book::name) = [红, 楼, 梦, 水, 浒, 传, 天, 龙, 八, 部, 无, 证, 之, 罪]
+//        listOf("abc","def").flatMap(String::toList) = [a, b, c, d, e, f]
+//        books.map(Book::authors).flatten() = [曹雪芹, 高鹗, 罗贯中, 施耐庵, 金庸, 倪匡, 紫金陈]
+//        sequence = kotlin.sequences.FilteringSequence
+//        map = chapter05lambda.Book@4769b07b
+//        filter = [曹雪芹, 高鹗]
+//        map = chapter05lambda.Book@cc34f4d
+//        filter = [罗贯中, 施耐庵]
+//        map = chapter05lambda.Book@17a7cec2
+//        filter = [金庸, 倪匡]
+//        map = chapter05lambda.Book@65b3120a
+//        filter = [紫金陈]
+//        sequence = [曹雪芹, 高鹗, 罗贯中, 施耐庵, 金庸, 倪匡]
+//        =======================================================
+//        map(1) filter(1) map(2) filter(4) map(3) filter(9) map(4) filter(16) map(5) filter(25) map(6) filter(36)
+//        =======================================================
+//        filter(1) filter(2) map(2) filter(3) filter(4) map(4) filter(5) filter(6) map(6)
+//        =======================================================
+//        map(1) map(2) map(3) map(4) map(5) map(6) filter(1) filter(4) filter(9) filter(16) filter(25) filter(36)
+//        =======================================================
+//        numbersTo100 = 4950
+//        File() = C:\Users\Administrator\IdeaProjects\KotlinInAction\.idea\libraries\KotlinJavaRuntime__2_.xml
+//        true
+//        false
+//        alphabet() = ABCDEFGHIJKLMNOPQRSTUVWXYZ
+//        Now I know the alphabet!
+//        alphabet1() = ABCDEFGHIJKLMNOPQRSTUVWXYZ
+//        Now I know the alphabet!
+//        alphabet2() = ABCDEFGHIJKLMNOPQRSTUVWXYZ
+//        Now I know the alphabet!
+//        alphabet3() = ABCDEFGHIJKLMNOPQRSTUVWXYZ
+//        Now I know the alphabet!
